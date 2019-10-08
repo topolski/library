5cd86cd9652099ea642f980b87e48876d0aa7eb2
Milanko Topolski <tope1991@gmail.com>
2019-10-08 18:26:36 +0200

    ui:auth and checkout/checkin feature test

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,15 @@
 |
 */
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::post('/books', 'BooksController@store');
 Route::patch('/books/{book}', 'BooksController@update');
 Route::delete('/books/{book}', 'BooksController@destroy');
 
 Route::post('/author', 'AuthorsController@store');
+
+Route::post('/checkout/{book}', 'CheckoutBookController@store');
+Route::post('/checkin/{book}', 'CheckinBookController@store');
